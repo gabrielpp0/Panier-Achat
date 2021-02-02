4dcd1d10b5df3d22cfdd80922b300da13f0ba646
gabrielpp0 <71361726+gabrielpp0@users.noreply.github.com>
2021-02-02 11:52:05 -0500

    avancement map / json

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,13 +1,20 @@
 import './ListeProduits.css';
+import Produit from './Produit';
+import tabProduits from './data/produits.json';
 
 export default function ListeProduits(props) {
+    console.log(tabProduits);
     return (
         <div className="ListeProduits">
             <h2>
                 Produits disponibles
             </h2>
             <ul>
-                
+                {
+                    tabProduits.map((elt) => 
+                        <Produit nom={elt.nom} prix={elt.prix} id={elt.id} />
+                    )
+                }
             </ul>
         </div>
     );
